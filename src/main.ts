918ff97618b3749de6734c9c2e55169bd4252506
tony <tony@kakashi>
2019-11-29 02:01:20 -0500

    update main

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
+
   const options = new DocumentBuilder()
     .setTitle('Payment Api')
     .setDescription('The payment API description')
