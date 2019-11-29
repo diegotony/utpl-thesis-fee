56a39aceb0dde69d2cfd6d112e4095411495487f
tony <tony@kakashi>
2019-11-29 02:41:38 -0500

    endopoint edited

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     .addTag('payment')
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('swagger/payment', app, document);
+  SwaggerModule.setup('swagger/fee', app, document);
   await app.listen(config.PORT);
 }
 bootstrap();
